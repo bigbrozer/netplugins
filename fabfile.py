91fb6dc15fe67999ff3ef163e1c39d20f6b59783
Vincent BESANCON <besancon.vincent@gmail.com>
2012-04-04 12:06:35 +0200

    Fabfile: missing imports

--- fabfile.py
+++ fabfile.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
-from fabric.api import task, cd, roles, put, run
+from fabric.api import task, cd, roles, hosts, put, run, local
 from monitoring.fabric import servers
 
 @task
