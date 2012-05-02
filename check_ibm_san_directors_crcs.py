c32c162a29f935f95f2d4ac00868cd23282ba04e
Vincent BESANCON <besancon.vincent@gmail.com>
2012-05-02 11:23:40 +0200

    check_ibm_san_directors_crcs.py: ready for testing.
    - Added exception handlers for retention file reading / saving.

--- check_ibm_san_directors_crcs.py
+++ check_ibm_san_directors_crcs.py
@@ -79,25 +79,47 @@ class IBMSanDirectorsCRC(NagiosPluginSNMP):
     def load_data(self):
         """Load pickled data."""
         logger.debug('-- Try to find pickle file \'%s\'...' % self.picklefile)
+
         if os.path.isfile(self.picklefile):
             logger.debug('\t - Pickle file is found, processing.')
-            with open(self.picklefile, 'rb') as pkl:
-                data = pickle.load(pkl)
+
+            data = list()
+            try:
+                with open(self.picklefile, 'rb') as pkl:
+                    data = pickle.load(pkl)
+            except (IOError, IndexError):
+                message = """Unable to read retention file !
+If you see this message that would mean that the retention file located in \'%s\' exists but it is not readable. Check
+permissions or try to delete it to generate a new one. It may be possible that this version of this plugin has
+changed and the retention file is outdated, so delete it if this is the case.
+
+%s
+""" % (self.picklefile, traceback.format_exc(limit=1))
+                plugin.unknown(message)
+
             logger.debug('\t - Pickle data found, loading %d records.' % len(data))
             return data
         else:
-            logger.debug('\t - Pickle file not found, continue.')
+            logger.debug('\t - Pickle file not found, creates.')
             return list()
 
     def save_data(self, data):
         """Save data into a pickle file."""
         logger.debug('-- Saving data to file \'%s\'...' % self.picklefile)
-        with open(self.picklefile, 'wb') as pkl:
-            # Avoid having a large pickle file if above 100 recorded values (plugin executions)
-            if len(data) > 100:
-                logger.debug('\t - Records limit reached, purging old records.')
-                del data[0]
-            pickle.dump(data, pkl)
+        try:
+            with open(self.picklefile, 'wb') as pkl:
+                # Avoid having a large pickle file if above 100 recorded values (plugin executions)
+                if len(data) > 100:
+                    logger.debug('\t - Records limit reached, purging old records.')
+                    del data[0]
+                pickle.dump(data, pkl)
+        except IOError:
+            message = """Unable to save retention file !
+If you see this message that would mean that the retention file located in \'%s\' is not writable. Check permissions.
+
+%s
+""" % (self.picklefile, traceback.format_exc(limit=1))
+            plugin.unknown(message)
 
 
 # Init plugin
@@ -158,8 +180,14 @@ port_stats = {}
 
 logger.debug('-- Processing pickled data for the last %d records.' % plugin.options.avgrec)
 last_records = retention_data[-plugin.options.avgrec:]
-avg_record_time = datetime.fromtimestamp(plugin.runtime) - datetime.fromtimestamp(last_records[0]['timestamp'])
-avg_record_time = int(math.ceil(avg_record_time.total_seconds()/60))
+
+# Calculate average time period
+if len(last_records) > 1:
+    avg_record_time = datetime.fromtimestamp(plugin.runtime) - datetime.fromtimestamp(last_records[0]['timestamp'])
+    avg_record_time = int(math.ceil(avg_record_time.total_seconds()/60))
+else:
+    avg_record_time = 0
+
 for data in last_records:
     val = data['values']
 
@@ -202,20 +230,22 @@ nbr_warn = len(errors['warning'])
 nbr_crit = len(errors['critical'])
 if not nbr_warn and not nbr_crit:
     nagios_status = plugin.ok
-    nagios_output = "No CRC error detected on ports. (Average on last %s mins)" % avg_record_time
+    nagios_output = "No CRC error detected on ports."
 elif nbr_warn and not nbr_crit:
     nagios_status = plugin.warning
-    nagios_output = "%d ports have warnings CRC errors ! (Average on last %s mins)\n" % (nbr_warn, avg_record_time)
+    nagios_output = "%d ports have warnings CRC errors !" % nbr_warn
 elif nbr_crit and not nbr_warn:
     nagios_status = plugin.critical
-    nagios_output = "%d ports have criticals CRC errors ! (Average on last %s mins)\n" % (nbr_crit, avg_record_time)
+    nagios_output = "%d ports have criticals CRC errors !" % nbr_crit
 elif nbr_warn and nbr_crit:
     nagios_status = plugin.critical
-    nagios_output = "%d ports have criticals, %d ports have warnings CRC errors ! (Average on last %s mins)\n" % (
-        nbr_crit,
-        nbr_warn,
-        avg_record_time
-        )
+    nagios_output = "%d ports have criticals, %d ports have warnings CRC errors !" % (nbr_crit, nbr_warn)
+
+# Show average record time if this is not the first run
+if avg_record_time:
+    nagios_output = "%s (Average on last %s mins)" % (nagios_output, avg_record_time)
+else:
+    nagios_output = "%s (First run)" % nagios_output
 
 # Check for errors details in long output
 for status in errors:
